4a407b040e7ee4913ab2d23da644cc43909f1307
Arun Chaitanya Jami <arunchaitanyajami@gmail.com>
2023-11-25 06:11:59 +0000

    Build changes from 6cea41d406d8bdc0172d32b0075e81bbad7592b6
    https://circleci.com/gh/arunchaitanyajami/acjwp-community-connector/126

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -22,4 +22,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit02ca7ab71731f801f326b5026a43fd22::getLoader();
+return ComposerAutoloaderInitb117a0ab1ab5493b1f98cd488381c431::getLoader();
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit02ca7ab71731f801f326b5026a43fd22
+class ComposerStaticInitb117a0ab1ab5493b1f98cd488381c431
 {
     public static $prefixLengthsPsr4 = array (
         'C' => 
@@ -140,9 +140,9 @@ class ComposerStaticInit02ca7ab71731f801f326b5026a43fd22
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit02ca7ab71731f801f326b5026a43fd22::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit02ca7ab71731f801f326b5026a43fd22::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInit02ca7ab71731f801f326b5026a43fd22::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitb117a0ab1ab5493b1f98cd488381c431::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitb117a0ab1ab5493b1f98cd488381c431::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInitb117a0ab1ab5493b1f98cd488381c431::$classMap;
 
         }, null, ClassLoader::class);
     }
